08a6ae501bbc7d93e20439b46a78bdc77f57124c
bruno0199 <brunocristian0199@gmail.com>
2021-02-11 22:11:36 -0300

    feat: create simple calculator in shell script and readme for implementation

---
+++ bruno/calculator/calculator.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# calculator.sh - Calculadora simples em shell script utilizando boas práticas
+#
+# Site:       https://github.com/bruno0199/devops-playground
+# Autor:      Bruno Cristian
+# Manutenção: Bruno Cristian
+#
+# ------------------------------------------------------------------------ #
+#  Este programa irá fazer cálculos matemáticos
+#
+#  Exemplos:
+#      $ ./calculator.sh
+#      
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 - versão básica
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   bash 5.0.17
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
+VALOR_1=0
+VALOR_2=0
+OPCAO=0
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- TESTES ----------------------------------------- #
+
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- FUNÇÕES ----------------------------------------- #
+menu() {
+  read -p "Escolha uma operação
+      1 - Somar
+      2 - Subtrair
+      3 - Multiplicar
+      4 - Divisão
+      > " OPCAO
+}
+
+executar() {
+  menu
+
+  case $OPCAO in
+    1)  soma ;;
+    2)  subtracao ;;
+    3)  multiplicacao ;;
+    4)  divisao ;;
+    *)  echo "Opção incorreta" ;;
+  esac
+}
+
+inserir_numeros() {
+  read -p "Digite um número: > " VALOR_1
+  read -p "Digite outro número: > " VALOR_2
+}
+
+soma() {
+  insercao_numeros
+  echo $(($VALOR_1 + $VALOR_2))
+}
+
+subtracao() {
+  insercao_numeros
+  echo $(($VALOR_1 - $VALOR_2))
+}
+
+multiplicacao() {
+  insercao_numeros
+  echo $(($VALOR_1 * $VALOR_2))
+}
+
+divisao() {
+  insercao_numeros
+  echo $(($VALOR_1 / $VALOR_2))
+}
+
+
+# ------------------------------------------------------------------------ #
+
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
+executar
+# ------------------------------------------------------------------------ #
